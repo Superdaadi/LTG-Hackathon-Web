08b851dc4733f9a8f12d449b8afbd81b0d889888
Superdaadi <dadimanuelsch@gmail.com>
2025-07-07 11:06:40 +0100

    7-7-2025

--- src/app/level/level.component.ts
+++ src/app/level/level.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-level',
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './level.component.css'
 })
 export class LevelComponent {
+  timeLeft: number = 0;
+  displayTime: string = '';
+  private intervalId: any;
 
+  ngOnInit(): void {
+    this.updateDisplayTime();
+
+    this.intervalId = setInterval(() => {
+      if (this.timeLeft >= 0) {
+        this.timeLeft ++;
+        this.updateDisplayTime();
+      } else {
+        clearInterval(this.intervalId);
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  updateDisplayTime(): void {
+    const minutes = Math.floor(this.timeLeft / 60);
+    const seconds = this.timeLeft % 60;
+    this.displayTime = `${this.pad(minutes)}:${this.pad(seconds)}`;
+  }
+
+  pad(value: number): string {
+    return value < 10 ? '0' + value : value.toString();
+  }
 }
